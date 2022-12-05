b1488a40d9723877f5f8cfa2f9bc5ccdd572d881
KrutoiProgram <Andrey23451dss2@gmail.com>
2022-12-05 13:30:34 +0500

    glv26doesn't work

--- app/src/main/java/com/example/photogallery/FlickrFetchr.kt
+++ app/src/main/java/com/example/photogallery/FlickrFetchr.kt
@@ -1,11 +1,17 @@
 package com.example.photogallery
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.util.Log
+import androidx.annotation.WorkerThread
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.photogallery.api.FlickrApi
 import com.example.photogallery.api.FlickrResponse
+import com.example.photogallery.api.PhotoInterceptor
 import com.example.photogallery.api.PhotoResponse
+import okhttp3.OkHttpClient
+import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -17,19 +23,29 @@ private const val TAG = "FlickrFetchr"
 class FlickrFetchr {
     private val flickrApi: FlickrApi
     init {
+        val client = OkHttpClient.Builder()
+            .addInterceptor(PhotoInterceptor())
+            .build()
         val retrofit: Retrofit =
             Retrofit.Builder()
                 .baseUrl("https://www.flickr.com/")
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
                 .build()
         flickrApi = retrofit.create(FlickrApi::class.java)
     }
     fun fetchPhotos(): LiveData<List<GalleryItem>> {
-        val responseLiveData:
-                MutableLiveData<List<GalleryItem>> = MutableLiveData()
+        return fetchPhotoMetadata(flickrApi.fetchPhotos())
+    }
+    fun searchPhotos(query: String): LiveData<List<GalleryItem>> {
+        return fetchPhotoMetadata(flickrApi.searchPhotos(query))
+    }
 
-        val flickrRequest: Call<FlickrResponse> =
-            flickrApi.fetchPhotos()
+    private fun
+            fetchPhotoMetadata(flickrRequest: Call<FlickrResponse>): LiveData<List<GalleryItem>> {
+        val responseLiveData: MutableLiveData<List<GalleryItem>> = MutableLiveData()
+
+
         flickrRequest.enqueue(object :
             Callback<FlickrResponse> {
             override fun onFailure(call:
@@ -59,5 +75,16 @@ class FlickrFetchr {
         })
         return responseLiveData
     }
-
+    @WorkerThread
+    fun fetchPhoto(url: String): Bitmap? {
+        val response: Response<ResponseBody> =
+            flickrApi.fetchUrlBytes(url).execute()
+        val bitmap =
+            response.body()?.byteStream()?.use(BitmapFactory::decodeStream)
+        Log.i(
+            TAG,
+            "Decoded bitmap=$bitmap from Response=$response"
+        )
+        return bitmap
+    }
 }
--- app/src/main/java/com/example/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/example/photogallery/PhotoGalleryFragment.kt
@@ -1,11 +1,15 @@
 package com.example.photogallery
 
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
@@ -23,13 +27,26 @@ private const val TAG = "PhotoGalleryFragment"
 class PhotoGalleryFragment : Fragment() {
     private lateinit var photoGalleryViewModel: PhotoGalleryViewModel
     private lateinit var photoRecyclerView: RecyclerView
+    private lateinit var thumbnailDownloader: ThumbnailDownloader<PhotoHolder>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        retainInstance = true
+        setHasOptionsMenu(true)
         photoGalleryViewModel =
             ViewModelProviders
                 .of(this)
                 .get(PhotoGalleryViewModel::class.java)
+        val responseHandler = Handler()
+        thumbnailDownloader =
+            ThumbnailDownloader(responseHandler) { photoHolder, bitmap ->
+                val drawable =
+                    BitmapDrawable(resources, bitmap)
+                photoHolder.bindDrawable(drawable)
+            }
+        lifecycle.addObserver(thumbnailDownloader.fragmentLifecycleObserver)
+
+
 
     }
 
@@ -38,6 +55,8 @@ class PhotoGalleryFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        viewLifecycleOwner.lifecycle.addObserver(
+            thumbnailDownloader.viewLifecycleObserver)
         val view =
             inflater.inflate(R.layout.fragment_photo_gallery, container, false)
         photoRecyclerView =
@@ -53,35 +72,57 @@ class PhotoGalleryFragment : Fragment() {
             viewLifecycleOwner,
             Observer { galleryItems ->
 
-                photoRecyclerView.adapter = PhotoHolder.PhotoAdapter(galleryItems)
+                photoRecyclerView.adapter = PhotoAdapter(galleryItems)
 
 
             })
     }
-    class PhotoHolder(itemTextView: TextView) : RecyclerView.ViewHolder(itemTextView)
-    {
-        val bindTitle: (CharSequence) -> Unit =
-            itemTextView::setText
-         class PhotoAdapter(private val galleryItems: List<GalleryItem>)
-            : RecyclerView.Adapter<PhotoHolder>() {
-            override fun onCreateViewHolder(
-                parent: ViewGroup,
-                viewType: Int
-            ): PhotoHolder {
-                val textView =
-                    TextView(parent.context)
-                return PhotoHolder(textView)
-            }
-            override fun getItemCount(): Int =
-                galleryItems.size
-            override fun onBindViewHolder(holder:
-                                          PhotoHolder, position: Int) {
-                val galleryItem =
-                    galleryItems[position]
-                holder.bindTitle(galleryItem.title)
-            }
-        }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        viewLifecycleOwner.lifecycle.removeObserver(
+            thumbnailDownloader.viewLifecycleObserver
+        )
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        lifecycle.removeObserver(thumbnailDownloader.fragmentLifecycleObserver)
+    }
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.fragment_photo_gallery, menu)
+    }
 
+    private class PhotoHolder(private val itemImageView: ImageView) : RecyclerView.ViewHolder(itemImageView) {
+        val bindDrawable: (Drawable) -> Unit = itemImageView::setImageDrawable
+    }
+
+    private inner class PhotoAdapter(private val galleryItems: List<GalleryItem>)
+        : RecyclerView.Adapter<PhotoHolder>() {
+        override fun onCreateViewHolder(
+            parent: ViewGroup,
+            viewType: Int
+        ): PhotoHolder {
+            val view = layoutInflater.inflate(
+                R.layout.list_item_gallery,
+                parent,
+                false
+            ) as ImageView
+            return PhotoHolder(view)
+        }
+        override fun getItemCount(): Int =
+            galleryItems.size
+        override fun onBindViewHolder(holder:
+                                      PhotoHolder, position: Int) {
+            val galleryItem =
+                galleryItems[position]
+            val placeholder: Drawable =
+                ContextCompat.getDrawable(
+                    requireContext(),
+                    R.drawable.whitesqr
+                ) ?: ColorDrawable()
+            holder.bindDrawable(placeholder)
+            thumbnailDownloader.queueThumbnail(holder, galleryItem.url)
+        }
     }
 
 
